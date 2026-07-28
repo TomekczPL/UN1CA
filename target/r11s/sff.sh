f6cdc7d611a80af4d4642fd5c79eaa5adf86e1a7
Tomek <132781414+TomekczPL@users.noreply.github.com>
2026-07-28 03:15:57 +0200

    r11s: init
    Adds the Galaxy S23 FE (Exynos 2200, SM-S711B) target
    
    Co-authored-by: Maja Błaszczak <maja.blaszczak@proton.me>
    Signed-off-by: Tomek <tomekczx@proton.me>

---
+++ target/r11s/sff.sh
@@ -0,0 +1,7 @@
+# Copyright (c) 2026 Salvo Giangreco
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# SEC Floating Feature configuration file for Galaxy S23 FE (Exynos) (r11s)
+
+# Enable extra brightness feature
+SEC_FLOATING_FEATURE_LCD_SUPPORT_EXTRA_BRIGHTNESS=TRUE
